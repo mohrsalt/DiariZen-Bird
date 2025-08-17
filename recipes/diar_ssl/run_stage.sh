033fccc950ca82250c8b7828a3584679956eb64c
mohrsalt <MOHOR001@e.ntu.edu.sg>
2025-08-17 22:54:46 +0800

    more changes

--- recipes/diar_ssl/run_stage.sh
+++ recipes/diar_ssl/run_stage.sh
@@ -11,7 +11,7 @@ module load miniforge3
 
 # Licensed under the MIT license.
 # Copyright 2024 Brno University of Technology (author: Jiangyu Han, ihan@fit.vut.cz)
-
+cd /home/users/ntu/bhargavi/Diarizen/recipes/diar_ssl
 #set -eu
 #ulimit -n 2048
 
@@ -35,20 +35,9 @@ dtype=test
 data_dir=$recipe_root/data/AMI_AliMeeting_AISHELL4
 seg_duration=8
 
-# clustering setup
-clustering_method=AgglomerativeClustering
-ahc_threshold=0.70
-min_cluster_size=30
-infer_affix=_constrained_AHC_thres_${ahc_threshold}_mcs_${min_cluster_size}
 
-avg_ckpt_num=5
-val_metric=Loss   # Loss or DER
-val_mode=best   # [prev, best, center]  
 
-# scoring setup
-collar=0
-REF_DIR=$data_dir
-dscore_dir=/home/users/ntu/bhargavi/DiariZen/dscore
+
 
 # =======================================
 # =======================================
@@ -66,37 +55,4 @@ if [ $stage -le 1 ]; then
     fi
 fi
 
-diarization_dir=$exp_root/$conf_name    # can be replaced by our pre-trained models, e.g. diarization_dir=/YOUR_PATH/checkpoints/wavlm_updated_conformer
-config_dir=`ls $diarization_dir/*.toml | sort -r | head -n 1`
-embedding_model=/YOUR_PATH/pretrained/pyannote3/wespeaker-voxceleb-resnet34-LM/pytorch_model.bin     # it's necessary to have "pyannote" in your directory path
 
-if [ $stage -le 2 ]; then
-    echo "stage2: model inference..."
-    export CUDA_VISIBLE_DEVICES=0
-
-    train_log=`du -h $diarization_dir/*.log | sort -rh | head -n 1 | awk '{print $NF}'`
-    cat $train_log | grep 'Loss/DER' | awk -F ']:' '{print $NF}' > $diarization_dir/val_metric_summary.lst
-
-    for dset in AMI AliMeeting AISHELL4; do
-        conda activate diarizen && python infer_avg.py -C $config_dir \
-            -i ${data_dir}/${dtype}/${dset}/wav.scp \
-            -o ${diarization_dir}/infer$infer_affix/metric_${val_metric}_${val_mode}/avg_ckpt${avg_ckpt_num}/${dtype}/${dset} \
-            --embedding_model $embedding_model \
-            --avg_ckpt_num $avg_ckpt_num \
-            --val_metric $val_metric \
-            --val_mode $val_mode \
-            --val_metric_summary $diarization_dir/val_metric_summary.lst \
-            --seg_duration $seg_duration \
-            --clustering_method $clustering_method \
-            --ahc_threshold $ahc_threshold \
-            --min_cluster_size $min_cluster_size 
-
-        echo "stage3: scoring..."
-        SYS_DIR=${diarization_dir}/infer$infer_affix/metric_${val_metric}_${val_mode}/avg_ckpt${avg_ckpt_num}
-        OUT_DIR=${SYS_DIR}/${dtype}/${dset}
-        conda activate diarizen && python ${dscore_dir}/score.py \
-            -r ${REF_DIR}/${dtype}/${dset}/rttm \
-            -s $OUT_DIR/*.rttm --collar ${collar} \
-            > $OUT_DIR/result_collar${collar}
-    done
-fi
